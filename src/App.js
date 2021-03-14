9d0da8169ccb73459dcd919a521fa2cc4f4bac2f
brycemccainc <brycemccainc@gmail.com>
2021-03-14 16:39:39 -0400

    Added Multiple Pages to App
    Changes:
    Added 3 pages, Home, About, And ViewEdit
    
    (Home) is the homepage will Cognito will be stored, code will have to be added later in order for users be prohibited from accessing view/edit without login.
    
    (About) will be information regarding what the app is used for and how people can use it + FAQ
    
    (Viewedit) will hold the code for a basic prototype view/edit system for this iteration.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import React from "react";
 import logo from './Screen Shot 2021-02-18 at 1.12.53 PM.png';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import ViewEdit from './ViewEdit';
+import { Route, Link } from 'react-router-dom';
+import NavBar from './NavBar';
+
 
 function App() {
   return (
     <div className="App">
+    <NavBar />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/viewedit" component={ViewEdit} />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           <code></code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
       </header>
     </div>
   );
---
+++ src/ViewEdit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ViewEdit(){
+return (
+<div>
+<h1> View and Edit Hierarchies </h1>
+</div>
+);
+}
+
+export default ViewEdit;
