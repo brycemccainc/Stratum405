ff2c34f0339ed7f92aa479ce5f3a2e5b0a0708c1
Shaylah0701 <shaylah0701@gmail.com>
2021-03-18 01:19:43 -0400

    ViewEdit changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from './Screen Shot 2021-02-18 at 1.12.53 PM.png';
 import './App.css';
 import Home from './Home';
 import About from './About';
@@ -16,9 +15,7 @@ function App() {
     <Route exact path="/about" component={About} />
     <Route exact path="/viewedit" component={ViewEdit} />
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+
     </div>
   );
 }
