59381314773a6e7237cac2ac2941b7d55a7740e1
brycemccainc <brycemccainc@gmail.com>
2021-04-21 22:50:25 -0400

    Update About.js

--- src/About.js
+++ src/About.js
@@ -4,6 +4,10 @@ function About(){
 return (
 <div>
 <h1>About</h1>
+<text>
+Stratum is a reporting hierarchy management tool that will allow companies to uniquely model their data according to specific business needs. Stratum is flexible enough to work for any type of hierarchy, within any given industry. Our self-service tool gives users the power to create and manage their own hierarchies. As businesses grow and requirements evolve, it’s important for organizations to have the ability to create structured relationships among data items that are often highly complex. It is important for a team to have robust software that is customizable enough to where it bends to the team's needs, while still holding a distinct user experience that sets the hierarchical software flow apart from competitors.
+
+</text>
 
 </div>
 );
